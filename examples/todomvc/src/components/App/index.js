c08248f09b090b559148b902fd609f5a6641b8e2
EGOIST <0x142857@gmail.com>
2018-03-14 11:39:02 +0800

    remove todo example

--- examples/todomvc/src/components/App/index.js
+++
@@ -1,9 +0,0 @@
-import { connect, mutation, state } from 'zerotwo'
-import App from './App.vue'
-
-export default connect({
-  todos: state(),
-  addTodo: mutation(),
-  clearCompleted: mutation(),
-  toggleAll: mutation()
-})(App)
--- examples/todomvc/src/components/Todo/index.js
+++
@@ -1,8 +0,0 @@
-import { connect, mutation } from 'zerotwo'
-import Todo from './Todo.vue'
-
-export default connect({
-  editTodo: mutation(),
-  toggleTodo: mutation(),
-  deleteTodo: mutation()
-}, Todo)
